f5902e1e1941845d3ad9e2727fec1c524c01f25f
yaofeng <12345678>
2020-05-04 19:29:30 +0800

    按照微信的方式调整juejin的输出

--- websites/juejin.rb
+++ websites/juejin.rb
@@ -8,7 +8,16 @@ register_website(
     title = document.css('.article-title').first.content
     author = document.css('.author-info-box').first.content
     content = document.css('.article').first
-
+    content.traverse{|x|
+      x.remove_class
+      x.remove_attribute('id')
+      x.remove_attribute('style')
+      if x.name == 'img'
+        unless x['src']
+          x['src'] = x['data-src']
+        end
+      end
+    }
     {
       title: title,
       author: author,
