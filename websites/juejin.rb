8cd484c907926b13a8d4b3374fbf5db2dd9186de
yaofeng <12345678>
2020-05-04 19:16:33 +0800

    add juejin

---
+++ websites/juejin.rb
@@ -0,0 +1,21 @@
+register_website(
+  name: 'juejin',
+  test: -> (uri) {
+    uri.hostname == 'juejin.im'
+  },
+  process: -> (html) {
+    document = Nokogiri::HTML(html)
+    title = document.css('.article-title').first.content
+    author = document.css('.author-info-box').first.content
+    content = document.css('.article').first
+
+    content.css('script').each(&:remove)
+    {
+      title: title,
+      author: author,
+      content: content.to_html.lines.map(&:strip).join
+    }
+	
+	
+  }
+)
