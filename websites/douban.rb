e4e0952793c845f002aa589cdffbaa7e16d53922
yaofeng <12345678>
2020-05-04 20:47:53 +0800

    add douban

---
+++ websites/douban.rb
@@ -0,0 +1,20 @@
+register_website(
+  name: 'douban',
+  test: -> (uri) {
+    uri.hostname == 'www.douban.com'
+  },
+  process: -> (html) {
+    document = Nokogiri::HTML(html)
+    title = document.css('.note-header').first.content
+    author = document.css('.note-author').first.content
+    content = document.css('.note-container').first
+ 
+    {
+      title: title,
+      author: author,
+      content: content.to_html.lines.map(&:strip).join
+    }
+	
+	
+  }
+)
