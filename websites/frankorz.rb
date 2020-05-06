c16598a7688d06159b8448a8a82e6d2fd92d4a02
yaofeng <12345678>
2020-05-06 17:46:10 +0800

    frankorz

---
+++ websites/frankorz.rb
@@ -0,0 +1,20 @@
+register_website(
+  name: 'frankorz',
+  test: -> (uri) {
+    uri.hostname == 'frankorz.com'
+  },
+  process: -> (html) {
+    document = Nokogiri::HTML(html)
+    title = document.css('.post__title').first.content
+    author = 'frankorz'
+    content = document.css('.post').first
+   
+    {
+      title: title,
+      author: author,
+      content: content.to_html.lines.map(&:strip).join
+    }
+	
+	
+  }
+)
